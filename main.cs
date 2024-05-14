43d23b9c3496ff58ccdc07c0aed87b16826ac48a
sansonc <sanson.cruz@gmail.com>
2024-05-14 16:12:18 +0000

    module 1

--- main.cs
+++ main.cs
@@ -2,6 +2,10 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
-    Console.WriteLine ("Hello World! Sanson is here");
+    Console.WriteLine ("Hello World! " + ConcatName("Sanson", "Cruz") + " is Here!");
+  }
+
+  private static string ConcatName(string firstname, string lastname) {
+    return firstname + " " + lastname;
   }
 }
